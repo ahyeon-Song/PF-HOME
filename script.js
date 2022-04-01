9329d8c07acaf9364d58c560352dd5b90ae5cb48
Ahyeon Song <sahjump@gmail.com>
2022-04-01 14:58:25 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+AOS.init(); 
+
+
+
+
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to(".skills", {
+//     scrollTrigger: {
+//       trigger: '.skills',
+//       start: 'bottom bottom',
+//       end: '.skills-wrap',
+//       scrub: 1,
+//       pin: ".skills",
+//       pinSpacing: true,
+//       toggleActions: 'play none none none',
+//     },
+//     opacity: 0.3
+//   }); 
+
+
+
+
+
+    
+
+  
